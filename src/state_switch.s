079d160b459e11ef4476ad19c686108129f85ae2
Cristobal Chinolla <c.chinolla@ttu.edu>
2021-07-30 11:46:59 -0600

    update

--- src/state_switch.s
+++ src/state_switch.s
@@ -16,20 +16,20 @@ jt:
 
 change_state:
 	cmp	#4, r12
-	jc	end
+	jc	default
 	add	r12, r12
 	mov	jt(r12), r0
 case0:
 	mov #0, &state
-	jmp end
+	jmp default
 case1:
 	mov #1, &state
-	jmp end
+	jmp default
 case2:
 	mov #2, &state
-	jmp end
+	jmp default
 case3:
 	mov #3, &state
-	jmp end
-end:
+	jmp default
+default:
 	ret
